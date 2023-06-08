2eff2c85eb9d530b4d82e05dfe70b6560ff6e780
GamingYoung <zzzz7057@163.com>
2023-06-08 19:46:17 +0800

    Layout

--- FrontEnd/src/pages/index.vue
+++ FrontEnd/src/pages/index.vue
@@ -14,36 +14,6 @@ function go() {
 </script>
 
 <template>
-<!--  <div>-->
-<!--    <div i-carbon-campsite inline-block text-4xl />-->
-<!--    <p>-->
-<!--      <a rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank">-->
-<!--        Vitesse Lite-->
-<!--      </a>-->
-<!--    </p>-->
-<!--    <p>-->
-<!--      <em text-sm op75>Opinionated Vite Starter Template</em>-->
-<!--    </p>-->
-
-<!--    <div py-4 />-->
-
-<!--    <TheInput-->
-<!--      v-model="name"-->
-<!--      placeholder="What's your name?"-->
-<!--      autocomplete="false"-->
-<!--      @keydown.enter="go"-->
-<!--    />-->
-
-<!--    <div>-->
-<!--      <button-->
-<!--        class="m-3 text-sm btn"-->
-<!--        :disabled="!name"-->
-<!--        @click="go"-->
-<!--      >-->
-<!--        Go-->
-<!--      </button>-->
-<!--    </div>-->
-<!--  </div>-->
   <div class="container">
     <div class="left-side">
       <div class="top-section">
@@ -62,22 +32,25 @@ function go() {
   </div>
 </template>
 
-<style>
+<style scoped>
 .container {
   display: flex;
+  max-width: 100%;
   height: 100%;
-  width: 100%;
+  margin: 0;
 }
 
 .left-side {
   flex: 3;
   display: flex;
+  margin: 0;
   flex-direction: column;
 }
 
 .right-side {
   flex: 1;
   background-color: green;
+  margin: 0;
 }
 
 .top-section {
